5bca50fdfb0806c728fe82f3344a7ae8a859a215
NicolGit <nicolgit@users.noreply.github.com>
2018-06-12 17:13:33 +0200

    APIScientificCalculator (core 2.1) first checkin

--- nicold.playground/nicold.APIScientificCalculator/Program.cs
+++ nicold.playground/nicold.APIScientificCalculator/Program.cs
@@ -8,20 +8,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace nicold.API_ScientificCalculator
+namespace nicold.APIScientificCalculator
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
