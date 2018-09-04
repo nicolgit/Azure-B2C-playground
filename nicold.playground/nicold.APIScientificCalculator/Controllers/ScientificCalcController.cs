2db8f24e48143471c8773b87efbb6c0d2dc646a1
NicolGit @ PC <nicolgit@users.noreply.github.com>
2018-09-04 11:36:24 +0200

    fixes on scientific calculator api

--- nicold.playground/nicold.APIScientificCalculator/Controllers/ScientificCalcController.cs
+++ nicold.playground/nicold.APIScientificCalculator/Controllers/ScientificCalcController.cs
@@ -19,7 +19,8 @@ namespace nicold.APIScientificCalculator.Controllers
     {
         private static readonly HttpClient _client = new HttpClient();
         private string CALL_MULTIPLY = "http://nicolapicalculator.azurewebsites.net/api/calc/multiply?param1={0}&param2={1}";
-        
+        private string CALL_SPLIT = "http://nicolapicalculator.azurewebsites.net/api/calc/split?param1={0}&param2={1}";
+
         public const string POWER = "power";
         public const string PERCENTAGE = "percentage";
         public const string FACTORIAL = "factorial";
@@ -43,13 +44,13 @@ namespace nicold.APIScientificCalculator.Controllers
                         break;
                     case PERCENTAGE:
                         result = await _multiply( param1, param2);
-                        result = await _multiply(result, 1.0 / 100.0);
+                        result = await _split(result, 100.0);
                         break;
                     case FACTORIAL:
                         result = 1;
-                        for (int i=0; i< param1; i++)
+                        for (int i=1; i<= param1; i++)
                         {
-                            result *= await _multiply(result, i);
+                            result = await _multiply(result, i);
                         }
                         break;
                     default:
@@ -75,6 +76,25 @@ namespace nicold.APIScientificCalculator.Controllers
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearer);
 
             var httpResponse = await _client.GetAsync(string.Format(CALL_MULTIPLY, param1, param2));
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                result = double.Parse(await httpResponse.Content.ReadAsStringAsync(), CultureInfo.InvariantCulture);
+            }
+            return result;
+        }
+
+        private async Task<double> _split(double param1, double param2)
+        {
+            var header_bearer = HttpContext.Request.Headers["Authorization"];
+            string bearer = header_bearer.FirstOrDefault().Split(" ")[1];
+
+            double result = 0;
+
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearer);
+
+            var httpResponse = await _client.GetAsync(string.Format(CALL_SPLIT, param1, param2));
 
             if (httpResponse.IsSuccessStatusCode)
             {
